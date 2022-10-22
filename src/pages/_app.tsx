343b88366e7219d8bf570e0fe99f7a74383f9d9b
Matheus Oliveira <contato.mths@outlook.com>
2022-10-22 10:37:38 -0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Flex } from "@chakra-ui/react"
+import { theme } from "../styles/theme"
+import { AppProps } from "next/app"
 
-import theme from '../theme'
-import { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
 }
-
-export default MyApp
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,20 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { ColorModeScript } from '@chakra-ui/react'
+import NextDocument, { Html, Head, Main, NextScript } from "next/document"
+import { ColorModeScript } from "@chakra-ui/react"
 
 export default class Document extends NextDocument {
   render() {
     return (
-      <Html>
-        <Head />
+      <Html lang="pt-BR">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
-          {/* Make Color mode to persists when you refresh the page. */}
-          <ColorModeScript />
+          <ColorModeScript initialColorMode="dark" />
           <Main />
           <NextScript />
         </body>
